f050ae8c617d2910eee3b42e77527183a4453908
Andy Yan <andy@Andys-MacBook-Air.local>
2020-03-31 17:29:44 -0400

    we created the line segment and the two arcs for our bisector

--- RulerCompass/LineSegmentBisecterView.swift
+++ RulerCompass/LineSegmentBisecterView.swift
@@ -11,11 +11,28 @@ import UIKit
 class LineSegmentBisecterView: UIView {
 
     override func draw(_ rect: CGRect) {
-        let pencil = UIBezierPath()
-        pencil.move(to: CGPoint(x: 100, y: 200))
-        pencil.addLine(to: CGPoint(x: 300, y: 200))
+        let lineSegmentPath = UIBezierPath()
+        lineSegmentPath.move(to: CGPoint(x: 200, y: 500))
+        lineSegmentPath.addLine(to: CGPoint(x: 500, y: 500))
         
-        pencil.stroke()
+        lineSegmentPath.move(to: CGPoint(x: 200, y: 475))
+        lineSegmentPath.addLine(to: CGPoint(x: 200, y: 525))
+        
+        lineSegmentPath.move(to: CGPoint(x: 500, y: 475))
+        lineSegmentPath.addLine(to: CGPoint(x: 500, y: 525))
+        
+        lineSegmentPath.lineWidth = 3
+        
+        lineSegmentPath.stroke()
+        
+        let compassArc1 = UIBezierPath(arcCenter: CGPoint(x: 200, y: 500), radius: 200, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: true)
+        #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).setStroke()
+        compassArc1.stroke()
+        
+        let comapssArc2 = UIBezierPath(arcCenter: CGPoint(x: 500, y: 500), radius: 200, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: false)
+        #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).setStroke()
+        comapssArc2.stroke()
+        
     }
     
 
