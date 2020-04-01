ab4184791e5a74a7c6bb9e578a6962a7ed09d4e7
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2020-04-01 21:54:17 +0800

    We created a view and added it onto the screen.

---
+++ RulerCompass/CanvasView.swift
@@ -0,0 +1,15 @@
+//
+//  CanvasView.swift
+//  RulerCompass
+//
+//  Created by Lambert Lin on 2020/4/1.
+//  Copyright © 2020 daobao. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+    override func draw(_ rect: CGRect) {
+        
+    }
+ }
