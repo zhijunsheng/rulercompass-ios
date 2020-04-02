f68be2843b51517d23433850bdd54b6589ebad98
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2020-04-02 16:03:23 -0500

    added Canvas with color backround

---
+++ RulerCompass/CanvasView.swift
@@ -0,0 +1,21 @@
+//
+//  CanvasView.swift
+//  RulerCompass
+//
+//  Created by Grace Huang on 4/2/20.
+//  Copyright © 2020 Grace Huang. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
