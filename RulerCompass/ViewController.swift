a1aaa24e63bc37c4b24f123b46a074f9951aa7ea
Andy Yan <andy@Andys-MacBook-Air.local>
2020-04-03 16:47:09 -0400

    we used our traditional way to create a view

--- RulerCompass/ViewController.swift
+++ RulerCompass/ViewController.swift
@@ -4,9 +4,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view = CanvasView()
-        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
 
 
