f8b2531c0e2c33d184b7d4751506ad886e991731
Andy Yan <andy@Andys-MacBook-Air.local>
2020-04-03 16:55:19 -0400

    we moved all of our project up

--- RulerCompass/CanvasView.swift
+++ RulerCompass/CanvasView.swift
@@ -18,29 +18,29 @@ class CanvasView: UIView {
     
     func drawLineSegment() {
          let lineSegmentPath = UIBezierPath()
-        lineSegmentPath.move(to: CGPoint(x: 200, y: 500))
-        lineSegmentPath.addLine(to: CGPoint(x: 500, y: 500))
+        lineSegmentPath.move(to: CGPoint(x: 200, y: 400))
+        lineSegmentPath.addLine(to: CGPoint(x: 500, y: 400))
         
-        lineSegmentPath.move(to: CGPoint(x: 200, y: 475))
-        lineSegmentPath.addLine(to: CGPoint(x: 200, y: 525))
+        lineSegmentPath.move(to: CGPoint(x: 200, y: 375))
+        lineSegmentPath.addLine(to: CGPoint(x: 200, y: 425))
         
-        lineSegmentPath.move(to: CGPoint(x: 500, y: 475))
-        lineSegmentPath.addLine(to: CGPoint(x: 500, y: 525))
+        lineSegmentPath.move(to: CGPoint(x: 500, y: 375))
+        lineSegmentPath.addLine(to: CGPoint(x: 500, y: 425))
         
         lineSegmentPath.lineWidth = 3
         
         lineSegmentPath.stroke()
         
         let pencil = UIBezierPath()
-        pencil.move(to: CGPoint(x: 350, y: 300))
-        pencil.addLine(to: CGPoint(x: 350, y: 700))
+        pencil.move(to: CGPoint(x: 350, y: 200))
+        pencil.addLine(to: CGPoint(x: 350, y: 600))
         pencil.stroke()
         
-        let compassArc1 = UIBezierPath(arcCenter: CGPoint(x: 200, y: 500), radius: 200, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: true)
+        let compassArc1 = UIBezierPath(arcCenter: CGPoint(x: 200, y: 400), radius: 200, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: true)
         #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).setStroke()
         compassArc1.stroke()
         
-        let comapssArc2 = UIBezierPath(arcCenter: CGPoint(x: 500, y: 500), radius: 200, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: false)
+        let comapssArc2 = UIBezierPath(arcCenter: CGPoint(x: 500, y: 400), radius: 200, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: false)
         #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).setStroke()
         comapssArc2.stroke()
     }
