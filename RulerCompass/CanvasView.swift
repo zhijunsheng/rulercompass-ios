92f72493fb7cd8dbff781fd094cd2315c881b63a
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2020-04-04 21:15:04 +0800

    We did about compass and ruler.

--- RulerCompass/CanvasView.swift
+++ RulerCompass/CanvasView.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class CanvasView: UIView {
     override func draw(_ rect: CGRect) {
+        let pencil = UIBezierPath()
+        pencil.move(to: CGPoint(x: 120 , y: bounds.height / 2))
+        pencil.addLine(to: CGPoint(x:bounds.width - 120 , y: bounds.height / 2))
+        pencil.stroke()
+       
+        let pen = UIBezierPath(arcCenter: CGPoint(x: 120 , y: bounds.height / 2), radius: 320, startAngle: -0.5 * CGFloat.pi, endAngle: CGFloat.pi * 0.5, clockwise: true)
+        pen.stroke()
         
+        let pe1 = UIBezierPath(arcCenter: CGPoint(x:bounds.width - 120 , y: bounds.height / 2), radius: 320, startAngle: -0.5 * CGFloat.pi, endAngle: CGFloat.pi * 0.5, clockwise: false)
+        pe1.stroke()
+        
+        let pe = UIBezierPath()
+        pe.move(to: CGPoint(x: bounds.width / 2 , y: bounds.height - 120))
+        pe.addLine(to: CGPoint(x: bounds.width / 2 , y: 120 ))
+        pe.stroke()
     }
  }
