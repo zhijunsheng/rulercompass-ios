aed363bf8d5001e5b8770834d4dca475b3a7e567
Elaine <elainexu392@gmail.com>
2020-04-04 10:46:52 -0400

    MAde the background colour change and somhow the lines didn't show up then it worked.

--- RulerCompass/ViewController.swift
+++ RulerCompass/ViewController.swift
@@ -4,8 +4,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = LineBisectingView()
-        view.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
     }
 
 
