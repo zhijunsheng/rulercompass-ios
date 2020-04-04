555795fb1a609e59b85188dc6d45f26f97078932
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2020-04-04 21:37:17 +0800

    We make the problem better

--- RulerCompass/CanvasView.swift
+++ RulerCompass/CanvasView.swift
@@ -13,17 +13,31 @@ class CanvasView: UIView {
         let pencil = UIBezierPath()
         pencil.move(to: CGPoint(x: 120 , y: bounds.height / 2))
         pencil.addLine(to: CGPoint(x:bounds.width - 120 , y: bounds.height / 2))
+        
+        pencil.move(to: CGPoint(x: 120 , y: bounds.height / 2 - 12))
+        pencil.addLine(to: CGPoint(x: 120 , y: bounds.height / 2 + 12))
+        
+        pencil.move(to: CGPoint(x: bounds.width - 120  , y: bounds.height / 2 - 12))
+        pencil.addLine(to: CGPoint(x: bounds.width - 120 , y: bounds.height / 2 + 12))
+        
+        
+        pencil.lineWidth = 5
         pencil.stroke()
-       
+        
+        #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).setStroke()
         let pen = UIBezierPath(arcCenter: CGPoint(x: 120 , y: bounds.height / 2), radius: 320, startAngle: -0.5 * CGFloat.pi, endAngle: CGFloat.pi * 0.5, clockwise: true)
+        pen.lineWidth = 3
         pen.stroke()
         
         let pe1 = UIBezierPath(arcCenter: CGPoint(x:bounds.width - 120 , y: bounds.height / 2), radius: 320, startAngle: -0.5 * CGFloat.pi, endAngle: CGFloat.pi * 0.5, clockwise: false)
+        pe1.lineWidth = 3
         pe1.stroke()
         
+        #colorLiteral(red: 0.9950962663, green: 0.1524247825, blue: 0.001851926907, alpha: 1).setStroke()
         let pe = UIBezierPath()
         pe.move(to: CGPoint(x: bounds.width / 2 , y: bounds.height - 120))
         pe.addLine(to: CGPoint(x: bounds.width / 2 , y: 120 ))
+        pe.lineWidth = 6
         pe.stroke()
     }
  }
