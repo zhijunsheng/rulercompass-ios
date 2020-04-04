df32945394116c2a3b3fba5b14183157faaa9d01
Elaine <elainexu392@gmail.com>
2020-04-04 10:29:47 -0400

    Made the code more organized and changed the colours and made some lines thicker

--- RulerCompass/LineBisectingView.swift
+++ RulerCompass/LineBisectingView.swift
@@ -11,28 +11,38 @@ import UIKit
 class LineBisectingView: UIView {
     override func draw(_ rect: CGRect) {
         let pencil = UIBezierPath()
-        let pencil1 = UIBezierPath()
-        
+      
         pencil.move(to: CGPoint(x: 270, y: 500))
         pencil.addLine(to: CGPoint(x: 470, y: 500))
-        pencil1.move(to: CGPoint(x: 370, y: 200))
-        pencil1.addLine(to: CGPoint(x: 370, y: 700))
+        
+        pencil.move(to: CGPoint(x: 270, y: 490))
+        pencil.addLine(to: CGPoint(x: 270, y: 510))
+        
+        pencil.move(to: CGPoint(x: 470, y: 490))
+        pencil.addLine(to: CGPoint(x: 470, y: 510))
+        
         pencil.lineWidth = 3
         pencil.stroke()
         
-        pencil1.move(to: CGPoint(x: 370, y: 200))
-        pencil1.addLine(to: CGPoint(x: 370, y: 700))
-        pencil.lineWidth = 1
-        pencil1.stroke()
+        // Answer (middle point)
+        let bisector = UIBezierPath()
+            
+        bisector.move(to: CGPoint(x: 370, y: 200))
+        bisector.addLine(to: CGPoint(x: 370, y: 700))
+        bisector.lineWidth = 2
+        #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1).setStroke()
+        bisector.stroke()
         
-        // negative
+        
         let halfCircleAllIn1 = UIBezierPath(arcCenter:CGPoint(x: 270, y: 500), radius: 120, startAngle: -0.5 * CGFloat.pi, endAngle: 0.5 * CGFloat.pi, clockwise: true)
-        
+        #colorLiteral(red: 0, green: 0.9914394021, blue: 1, alpha: 1).setStroke()
+        halfCircleAllIn1.lineWidth = 5
         halfCircleAllIn1.stroke()
         
         
         let halfCircle2 = UIBezierPath(arcCenter: CGPoint(x: 470, y: 500), radius: 120, startAngle: -0.5 * CGFloat.pi, endAngle: 0.5 * CGFloat.pi, clockwise: false)
-        
+        #colorLiteral(red: 0.6177716851, green: 0.9856142402, blue: 0.7465093136, alpha: 1).setStroke()
+        halfCircle2.lineWidth = 5
         halfCircle2.stroke()
         
     }
