3d129d8d43c40ebe6c7c07dc9665694292822f1d
Andy Yan <andy@Andys-MacBook-Air.local>
2020-04-05 17:16:02 -0400

    we created colors for the different lines inside of our line bisector project

--- RulerCompass/CanvasView.swift
+++ RulerCompass/CanvasView.swift
@@ -19,19 +19,28 @@ class CanvasView: UIView {
     func drawAngle() {
         
         // angle
+        
+        
+        
         let pencil = UIBezierPath()
+        
         pencil.move(to: CGPoint(x: 300, y: 400))
         pencil.addLine(to: CGPoint(x: 400, y: 200))
         
         pencil.move(to: CGPoint(x: 300, y: 400))
         pencil.addLine(to: CGPoint(x: 500, y: 400))
         
-       
-        
+        pencil.lineWidth = 3
         pencil.stroke()
         
-        //arcs
-        let arc = UIBezierPath(arcCenter: CGPoint(x: 300, y: 400), radius: 150, startAngle: 0, endAngle: CGFloat.pi * 1.65, clockwise: false)
+    
+        // how many πs are there is number 100?
+        // 100 / 3.14
+        // 0.5 / 3.14 => 0.5 / 3 => 5/30 => 1/6
+        // roughly how many πs are there in 0.5?
+        //arcs 6 * 0.5 =
+        let arc = UIBezierPath(arcCenter: CGPoint(x: 300, y: 400), radius: 150, startAngle: CGFloat.pi * 0.25, endAngle: -CGFloat.pi *  0.5, clockwise: false)
+        #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1).setStroke()
         arc.stroke()
         
         let arc2 = UIBezierPath(arcCenter: CGPoint(x: 450, y: 400), radius: 150, startAngle: CGFloat.pi * 1.75, endAngle: CGFloat.pi * 1.6, clockwise: false)
@@ -40,10 +49,14 @@ class CanvasView: UIView {
         let arc3 = UIBezierPath(arcCenter: CGPoint(x: 375, y: 275), radius: 150, startAngle: CGFloat.pi * 1.9, endAngle: CGFloat.pi * 0.06, clockwise: true)
         arc3.stroke()
         
-        pencil.move(to: CGPoint(x: 300, y: 400))
-        pencil.addLine(to: CGPoint(x: 550, y: 255))
+       
+        let pencil2 = UIBezierPath()
         
-        pencil.stroke()
+        pencil2.move(to: CGPoint(x: 300, y: 400))
+        pencil2.addLine(to: CGPoint(x: 550, y: 255))
+        pencil2.lineWidth = 3
+        #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1).setStroke()
+        pencil2.stroke()
         
     }
     
@@ -54,6 +67,8 @@ class CanvasView: UIView {
         
         lineSegmentPath.move(to: CGPoint(x: 200, y: 375))
         lineSegmentPath.addLine(to: CGPoint(x: 200, y: 425))
+        
+        
         
         lineSegmentPath.move(to: CGPoint(x: 500, y: 375))
         lineSegmentPath.addLine(to: CGPoint(x: 500, y: 425))
@@ -74,6 +89,8 @@ class CanvasView: UIView {
         let comapssArc2 = UIBezierPath(arcCenter: CGPoint(x: 500, y: 400), radius: 200, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: false)
         #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).setStroke()
         comapssArc2.stroke()
+        
+        
     }
 
 
