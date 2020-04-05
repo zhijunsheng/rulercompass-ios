f9863fb7d2928b41b82a9a7b21bc1a0da303a4d5
Elaine <elainexu392@gmail.com>
2020-04-05 19:32:00 -0400

    MAde the angle bisector aka the straight line with a semi circle and a x.

--- RulerCompass/LineBisectingView.swift
+++ RulerCompass/LineBisectingView.swift
@@ -11,11 +11,32 @@ import UIKit
 class LineBisectingView: UIView {
     
     override func draw(_ rect: CGRect) {
-        lineBisecting()
-        
+        //        lineBisecting()
+        angleBisecting()
     }
     func angleBisecting() {
-         
+        let pencil = UIBezierPath()
+        
+        pencil.move(to: CGPoint(x: 500, y: 175))
+        pencil.addLine(to: CGPoint(x: 470, y: 225))
+        
+        pencil.move(to: CGPoint(x: 470, y: 170))
+        pencil.addLine(to: CGPoint(x: 500, y: 220))
+        
+        pencil.move(to: CGPoint(x: 270, y: 200))
+        pencil.addLine(to: CGPoint(x: 470, y: 100))
+        
+        pencil.move(to: CGPoint(x: 270, y: 200))
+        pencil.addLine(to: CGPoint(x: 670, y: 200))
+        
+        pencil.move(to: CGPoint(x: 270, y: 200))
+        pencil.addLine(to: CGPoint(x: 470, y: 300))
+        pencil.stroke()
+        
+        let halfCircleAllIn1 = UIBezierPath(arcCenter:CGPoint(x: 270, y: 200), radius: 120, startAngle: -0.5 * CGFloat.pi, endAngle: 0.5 * CGFloat.pi, clockwise: true)
+           #colorLiteral(red: 0, green: 0.9914394021, blue: 1, alpha: 1).setStroke()
+           halfCircleAllIn1.lineWidth = 5
+           halfCircleAllIn1.stroke()
     }
     func lineBisecting()  {
         let pencil = UIBezierPath()
