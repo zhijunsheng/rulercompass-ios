beb051e8d8d9a290aba8af4c7bc9a15eeeb99d19
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2020-04-05 19:58:05 -0500

    fixed line segment bisector and added color

--- RulerCompass/CanvasView.swift
+++ RulerCompass/CanvasView.swift
@@ -17,28 +17,34 @@ class CanvasView: UIView {
         let ruler = UIBezierPath()
         let compass = UIBezierPath()
         
-        ruler.move(to: CGPoint(x: 100, y: 400))
-        ruler.addLine(to: CGPoint(x: 600, y: 400))
-        ruler.move(to: CGPoint(x: 200, y: 390))
-        ruler.addLine(to: CGPoint(x: 200, y: 410))
+        ruler.move(to: CGPoint(x: 118, y: 400))
+        ruler.addLine(to: CGPoint(x: 400, y: 400))
+    
+        ruler.move(to: CGPoint(x: 118, y: 390))
+        ruler.addLine(to: CGPoint(x: 118, y: 410))
+        ruler.move(to: CGPoint(x: 400, y: 390))
+        ruler.addLine(to: CGPoint(x: 400, y: 410))
         
-        ruler.move(to: CGPoint(x: 260, y: 390))
-        ruler.addLine(to: CGPoint(x: 260, y: 410))
-        ruler.move(to: CGPoint(x: 320, y: 390))
-        ruler.addLine(to: CGPoint(x: 320, y: 410))
-
+        ruler.lineWidth = 3
         ruler.stroke()
         
-        compass.addArc(withCenter: CGPoint(x: 200, y: 400), radius: 200, startAngle: 1.5 * CGFloat.pi, endAngle: 2.5 * CGFloat.pi, clockwise: true)
+        compass.addArc(withCenter: CGPoint(x: 118, y: 400), radius: 200, startAngle: 1.5 * CGFloat.pi, endAngle: 2.5 * CGFloat.pi, clockwise: true)
         
-//        compass.move(to: CGPoint(x: 260, y: 400))
-        compass.addArc(withCenter: CGPoint(x: 320, y: 400), radius: 200, startAngle: 0.5 * CGFloat.pi, endAngle: 1.5 * CGFloat.pi, clockwise: true)
+        compass.move(to: CGPoint(x: 400, y: 600))
+        compass.addArc(withCenter: CGPoint(x: 400, y: 400), radius: 200, startAngle: 0.5 * CGFloat.pi, endAngle: 1.5 * CGFloat.pi, clockwise: true)
+        
+        #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1).setStroke()
+        compass.stroke()
         
         ruler.move(to: CGPoint(x: 260, y: 200))
         ruler.addLine(to: CGPoint(x: 260, y: 610))
         
+        ruler.lineWidth = 2
+        #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1).setStroke()
+        
         ruler.stroke()
         
-        compass.stroke()
+        
+        
     }
 }
