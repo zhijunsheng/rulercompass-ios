565aa3ec9f9ed92b852d997a732195ff1f25368b
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2020-04-05 19:32:50 -0500

    drew line segment bisector

--- RulerCompass/CanvasView.swift
+++ RulerCompass/CanvasView.swift
@@ -9,13 +9,36 @@
 import UIKit
 
 class CanvasView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        drawLineSegmentBisector()
     }
-    */
+    
+    func drawLineSegmentBisector() {
+        let ruler = UIBezierPath()
+        let compass = UIBezierPath()
+        
+        ruler.move(to: CGPoint(x: 100, y: 400))
+        ruler.addLine(to: CGPoint(x: 600, y: 400))
+        ruler.move(to: CGPoint(x: 200, y: 390))
+        ruler.addLine(to: CGPoint(x: 200, y: 410))
+        
+        ruler.move(to: CGPoint(x: 260, y: 390))
+        ruler.addLine(to: CGPoint(x: 260, y: 410))
+        ruler.move(to: CGPoint(x: 320, y: 390))
+        ruler.addLine(to: CGPoint(x: 320, y: 410))
 
+        ruler.stroke()
+        
+        compass.addArc(withCenter: CGPoint(x: 200, y: 400), radius: 200, startAngle: 1.5 * CGFloat.pi, endAngle: 2.5 * CGFloat.pi, clockwise: true)
+        
+//        compass.move(to: CGPoint(x: 260, y: 400))
+        compass.addArc(withCenter: CGPoint(x: 320, y: 400), radius: 200, startAngle: 0.5 * CGFloat.pi, endAngle: 1.5 * CGFloat.pi, clockwise: true)
+        
+        ruler.move(to: CGPoint(x: 260, y: 200))
+        ruler.addLine(to: CGPoint(x: 260, y: 610))
+        
+        ruler.stroke()
+        
+        compass.stroke()
+    }
 }
