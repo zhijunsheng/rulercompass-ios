30e8fb305656d14adadac9b39e41247337cae672
Bowen Lin <bowenlin@lins-MacBook-Pro.local>
2020-05-01 15:13:17 -0400

    Created new xcode project

--- RulerCompass/ViewController.swift
+++ RulerCompass/ViewController.swift
@@ -1,10 +1,23 @@
+//
+//  ViewController.swift
+//  RulerCompass
+//
+//  Created by Bowen Lin on 2020-05-01.
+//  Copyright © 2020 Bowen Lin. All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
