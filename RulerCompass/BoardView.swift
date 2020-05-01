90224f419ce0eacc9c5bdc29fa33da4886fc028b
Bowen Lin <bowenlin@lins-MacBook-Pro.local>
2020-05-01 15:21:04 -0400

    added BoardView and view on MainStoryBoard

---
+++ RulerCompass/BoardView.swift
@@ -0,0 +1,18 @@
+//
+//  BoardView.swift
+//  RulerCompass
+//
+//  Created by Bowen Lin on 2020-05-01.
+//  Copyright © 2020 Bowen Lin. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
