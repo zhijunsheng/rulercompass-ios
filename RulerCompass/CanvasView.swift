8bce9f502297806eb66ddbe382749428bb21857b
吴淑雅 <wushuya@miyabis-MacBook-Air.local>
2021-10-22 17:56:54 -0400

    We are finally done!

--- RulerCompass/CanvasView.swift
+++ RulerCompass/CanvasView.swift
@@ -12,6 +12,7 @@ class CanvasView: UIView {
     override func draw(_ rect: CGRect) {
 //        drawLineSegmentBisector()
         drawAngleBisector()
+        drawCrosshair(x: 311, y: 250)
     }
     
     func drawAngleBisector() {
@@ -21,7 +22,7 @@ class CanvasView: UIView {
         line.addLine(to: CGPoint(x: 300, y: 200))
         
         line.move(to: CGPoint(x: 100, y: 300))
-        line.addLine(to: CGPoint(x: 300, y: 295))
+        line.addLine(to: CGPoint(x: 300, y: 300))
         
         line.stroke()
         
@@ -30,6 +31,25 @@ class CanvasView: UIView {
         UIColor.red.setStroke()
         circle.lineWidth = 3
         circle.stroke()
+        
+        let circle2 = UIBezierPath(arcCenter: CGPoint(x: 280, y: 300), radius: 60, startAngle: CGFloat.pi * 0, endAngle: CGFloat.pi * 2, clockwise: false)
+        
+        UIColor.darkGray.setStroke()
+        circle2.stroke()
+        
+        let circle3 = UIBezierPath(arcCenter: CGPoint(x: 260, y: 218), radius: 60, startAngle: CGFloat.pi * 0, endAngle: CGFloat.pi * 2, clockwise: false)
+        
+        UIColor.darkGray.setStroke()
+        circle3.stroke()
+        
+        let line4 = UIBezierPath()
+        
+        line4.move(to: CGPoint(x: 100, y: 300))
+        line4.addLine(to: CGPoint(x: 311, y: 250))
+        
+        line4.lineWidth = 3
+        UIColor.blue.setStroke()
+        line4.stroke()
     }
     
     func drawLineSegmentBisector() {
@@ -67,6 +87,19 @@ class CanvasView: UIView {
         line2.lineWidth = 3
         UIColor.blue.setStroke()
         line2.stroke()
+    }
+    func drawCrosshair(x: CGFloat, y: CGFloat) {
+        let sniperThing = UIBezierPath()
+        
+        sniperThing.move(to: CGPoint(x: x - 10, y: y))
+        sniperThing.addLine(to: CGPoint(x: x + 10, y: y))
+        
+        sniperThing.move(to: CGPoint(x: x, y: y - 10))
+        sniperThing.addLine(to: CGPoint(x: x, y: y + 10))
+        
+        sniperThing.lineWidth = 2
+        UIColor.red.setStroke()
+        sniperThing.stroke()
     }
 }
 
