0e57c391734aab39c143317574914109edc8fafb
jichengye <jichengye@jichengyedeMacBook-Pro.local>
2021-11-13 20:27:49 +0800

    A new file is done.

---
+++ RulerCompass/CanvasView.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasView.swift
+//  RulerCompass
+//
+//  Created by jichengye on 2021/11/13.
+//  Copyright © 2021 daobao. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+}
