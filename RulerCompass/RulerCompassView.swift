d06ee24c394ee77bc64de4cfbe286bb9de01981c
Kenneth Wu <kennethwu@Kenneths-MBP.hitronhub.home>
2022-05-26 17:16:15 -0400

    today I calculated exactly the half of an angle

--- RulerCompass/RulerCompassView.swift
+++ RulerCompass/RulerCompassView.swift
@@ -10,6 +10,49 @@ import UIKit
 
 class RulerCompassView: UIView {
     override func draw(_ rect: CGRect) {
+//        lineSegmentCenter
+        angleBisector()
+    }
+    
+    func angleBisector() {
+        let pencil1 = UIBezierPath()
+        pencil1.move(to: CGPoint(x: 400, y: 400))
+        pencil1.addLine(to: CGPoint(x: 200, y: 400))
+        pencil1.addLine(to: CGPoint(x: 400, y: 300))
+        pencil1.lineWidth = 5
+        pencil1.stroke()
+        
+        let arc1 = UIBezierPath(arcCenter: CGPoint(x: 200, y: 400), radius: 150, startAngle: 0.2, endAngle: -0.2 * CGFloat.pi, clockwise: false)
+        UIColor.blue.setStroke()
+        arc1.stroke()
+        
+        let angl = atan(100.0/200.0)
+        let h = sin(angl) * 150
+        let b = cos(angl) * 150
+        let p1X = 200 + b
+        let p1Y = 400 - h
+        let p2X = 350
+        let p2Y = 400
+        
+        let arc2 = UIBezierPath(arcCenter: CGPoint(x: p2X, y: p2Y), radius: 75, startAngle: -0.35 * CGFloat.pi, endAngle: -0.1 * CGFloat.pi, clockwise: true)
+        arc2.stroke()
+
+        let arc3 = UIBezierPath(arcCenter: CGPoint(x: p1X, y: p1Y), radius: 75, startAngle: 0.2 * CGFloat.pi, endAngle: 0, clockwise: false)
+        arc3.stroke()
+        
+        let pencil2 = UIBezierPath()
+        let angl2 = angl / 2
+        let p3X: CGFloat = 600
+        let p3Y = 400 - tan(angl2) * 400
+        
+        pencil2.move(to: CGPoint(x: 200, y: 400))
+        pencil2.addLine(to: CGPoint(x: p3X, y: p3Y))
+        
+        UIColor.red.setStroke()
+        pencil2.stroke()
+    }
+    
+    func lineSegmentCenter() {
         let pencil1 = UIBezierPath()
         pencil1.move(to: CGPoint(x: 200, y: 350))
         pencil1.addLine(to: CGPoint(x: 500, y: 350))
@@ -30,7 +73,5 @@ class RulerCompassView: UIView {
         pencil2.addLine(to: CGPoint(x: 350, y: 600))
         UIColor.red.setStroke()
         pencil2.stroke()
-        
-        
     }
 }
